e706c7085b9da73d1557581abbd551240e70496a
6cdh <cd6cdh@gmail.com>
2021-09-23 15:38:11 +0800

    update(ext.SimpleMermaid.js): better error report

--- resources/ext.SimpleMermaid.js
+++ resources/ext.SimpleMermaid.js
@@ -3,6 +3,15 @@ mw.hook("wikipage.content").add(function ($content) {
   script.src = "https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js";
   script.async = true;
   script.onload = function () {
+    mermaid.parseError = function (err, hash) {
+      console.error(err);
+      var pre = document.createElement("pre");
+      pre.textContent = err;
+      var targetElement = document
+        .getElementsByClassName("error-icon")[0]
+        .closest("svg").parentElement;
+      targetElement.appendChild(pre);
+    };
     mermaid.initialize({
       startOnLoad: true,
       theme: mw.config.get("wgSimpleMermaidTheme"),
