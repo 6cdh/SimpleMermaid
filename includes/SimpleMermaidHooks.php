0d806518c249cc178faf6712f12e61d49bedddc6
6cdh <cd6cdh@gmail.com>
2022-07-20 09:12:52 +0800

    fix: use of ParserOutput::addModules with non-array argument in MW 1.38

--- includes/SimpleMermaidHooks.php
+++ includes/SimpleMermaidHooks.php
@@ -1,18 +1,21 @@
 <?php
-class SimpleMermaidHooks {
-    public static function onParserFirstCallInit( Parser $parser ) {
+class SimpleMermaidHooks
+{
+    public static function onParserFirstCallInit(Parser $parser)
+    {
         global $wgOut, $wgSimpleMermaidTheme;
 
-        $wgOut->addJsConfigVars( 'wgSimpleMermaidTheme', $wgSimpleMermaidTheme);
+        $wgOut->addJsConfigVars('wgSimpleMermaidTheme', $wgSimpleMermaidTheme);
 
-        $parser->setHook( 'mermaid', __CLASS__ . '::renderMermaid' );
+        $parser->setHook('mermaid', __CLASS__ . '::renderMermaid');
     }
 
-    public static function renderMermaid($input, array $args, Parser $parser, PPFrame $frame) {
+    public static function renderMermaid($input, array $args, Parser $parser, PPFrame $frame)
+    {
         global $wgSimpleMermaidTheme;
-        $parser->getOutput()->addModules( 'ext.SimpleMermaid' );
-        $attributes = [ "class" => "mermaid" ];
-        $element = Html::Element( "div", $attributes, $input);
+        $parser->getOutput()->addModules(['ext.SimpleMermaid']);
+        $attributes = ["class" => "mermaid"];
+        $element = Html::Element("div", $attributes, $input);
         return [$element, 'markerType' => 'nowiki'];
     }
 }
